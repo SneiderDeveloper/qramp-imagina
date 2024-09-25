ae6d696d5bac95c12101d75617e68dba2b5f91e0
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-09-25 14:15:59 -0500

    Adjusting UI

--- _components/scheduleKanban/components/scheduleFields.vue
+++ _components/scheduleKanban/components/scheduleFields.vue
@@ -3,9 +3,10 @@
     'lg:tw-flex-wrap lg:tw-space-x-0 xl:tw-flex xl:tw-space-x-2' : inlineMode,
     'tw-mb-1': !inlineMode
   }">
-    <div v-for="(field, keyField) in fields.form" :key="keyField">
+    <template v-for="(field, keyField) in fields.form" :key="keyField">
       <dynamic-field
         v-if="keyField !== 'sta' && keyField !== 'outboundScheduledDeparture'"
+        v-show="field.props?.vIf !== false"
         :field="field"
         v-model="form[keyField]"
         @update:modelValue="zanetizeData(keyField)"
@@ -13,23 +14,21 @@
          'tw-hidden': keyField === 'stationId',
          'xl:tw-w-fit' : inlineMode }"
       />
-      <div v-if="isbound.inbound && keyField === 'sta'">
-        <dynamic-field
+      <dynamic-field
+        v-if="isbound.inbound && keyField === 'sta'"
+        :field="field"
+        v-model="form[keyField]"
+        @update:modelValue="zanetizeData(keyField)"
+        :class="{ 'tw-hidden': keyField === 'stationId' }"
+      />
+      <dynamic-field
+          v-if="isbound.outbound && keyField === 'outboundScheduledDeparture'"
           :field="field"
           v-model="form[keyField]"
           @update:modelValue="zanetizeData(keyField)"
           :class="{ 'tw-hidden': keyField === 'stationId' }"
         />
-      </div>
-      <div v-if="isbound.outbound && keyField === 'outboundScheduledDeparture'">
-        <dynamic-field
-            :field="field"
-            v-model="form[keyField]"
-            @update:modelValue="zanetizeData(keyField)"
-            :class="{ 'tw-hidden': keyField === 'stationId' }"
-          />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
